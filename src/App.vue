fd368af3928a434bb1e2711b83ee7e6c3b3c2aad
Alexandr <toledospb@gmail.com>
2018-05-23 08:57:47 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-screen">
+    <div class="top">
+      <div class="search-icon-area">
+          <i class="fa fa-search icon-search fa-6x"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+
+  .main-screen {
+    background: #fde152;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    min-width: 760px;
+  }
+
+  .top {
+    background: #ff9d00;
+    width: 100%;
+    height: 100px;
+    top: 0;
+  }
+  .search-icon-area {
+    background: #ff8800;
+    width: 100px;
+    height: 100%;
+    position: relative;
+    left: 100px;
+  }
+
+  .icon-search {
+    color: #ff5100;
+    left: 46%;
+    top: 44%;
+    margin-right: -50%;
+    position: relative;
+  }
+</style>
